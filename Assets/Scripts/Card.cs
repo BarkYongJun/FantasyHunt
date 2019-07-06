9262c6a7388abc6cbbbb26c9d1cebe86258395d1
박용준 <>
2019-07-06 18:29:56 +0900

    카드 스크립트 추가, 유니티 복습 필요...

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public uint attack = 0;
+    public uint defence = 0;
+    public uint hp = 0;
+    public float attackDelay = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void InitCardData( uint valueAttack, uint valueDefence, uint valueHp, float valueDelay  )
+    {
+        attack = valueAttack;
+        defence = valueDefence;
+        hp = valueHp;
+        attackDelay = valueDelay;
+    }
+
+    void Attack()
+    {
+
+    }
+}
